8498d0a823b8a50d5b1c5bed01d738ccb9c29974
Tiago <tiago.romano@techne.com.br>
2018-12-11 18:57:40 +0000

    WIP

--- src/main/java/blockly/Util.java
+++ src/main/java/blockly/Util.java
@@ -31,5 +31,27 @@ public static Var processCurriculum(Var Entidade) throws Exception {
  }.call();
 }
 
+/**
+ */
+// Descreva esta função...
+public static void checkRedirectSimpleUser() throws Exception {
+  new Callable<Var>() {
+
+   private Var login = Var.VAR_NULL;
+   private Var item = Var.VAR_NULL;
+   private Var usuarios = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+    login = cronapi.util.Operations.getCurrentUserName();
+    item = cronapi.database.Operations.query(Var.valueOf("app.entity.Role"),Var.valueOf("select r from Role r where r.user.login = :userLogin"),Var.valueOf("userLogin",login));
+    if (cronapi.list.Operations.isEmpty(item).getObjectAsBoolean()) {
+        usuarios = cronapi.database.Operations.query(Var.valueOf("app.entity.User"),Var.valueOf("select u from User u where u.login = :login"),Var.valueOf("login",login));
+        cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeView"), Var.valueOf("#/home/logged/candidate-simple"), cronapi.list.Operations.newList(Var.valueOf("userId",cronapi.object.Operations.getObjectField(cronapi.list.Operations.get(usuarios, Var.valueOf(1)), Var.valueOf("id")))));
+    }
+   return Var.VAR_NULL;
+   }
+ }.call();
+}
+
 }
 
